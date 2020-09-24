fb296b068a7950381629939e9594aed3a78fcbab
ianruh <ianruh2@gmail.com>
2020-09-24 13:25:55 -0500

    Remove build

--- build/basic_motors/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ME439/src/basic_motors
-# Build directory: /root/ME439/build/basic_motors
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
