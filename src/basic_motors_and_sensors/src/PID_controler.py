f26fa85db2a85086d86fd955698576bf089d89d5
Sina <ssadeghian@wisc.edu>
2020-12-12 19:46:07 -0600

    Final

--- src/basic_motors_and_sensors/src/PID_controler.py
+++ src/basic_motors_and_sensors/src/PID_controler.py
@@ -1,13 +1,6 @@
-
-#PID control
-# param error: from SensorsToMotor.py
-# param K_p : yaml file defined in SensorsToMotor.py
-# param K_d : yaml file defined in SensorsToMotor.py
-
 class robot_PID():
-      
-    def __init__(self, error,K_p,K_d):
         
+    def __init__(self, error,K_p,K_d):
         self.K_p = K_p
         self.K_d = K_d
         self.error = error
@@ -15,11 +8,7 @@ class robot_PID():
         self.PID_val = 0
         
     def PIDcontrol(self):
-        
-        P = self.error # Propotional Error
-        
-        D = self.error - self.prev_error # Derivative Error
-        
-        self.PID_val = self.K_p * P + self.K_d * D #total error
-        
+        P = self.error
+        D = self.error - self.prev_error
+        self.PID_val = self.K_p * P + self.K_d * D 
         self.prev_error = self.error
